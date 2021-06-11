ba47c0038206b4457fe22df995fd4b001a37463d
unknown <kimitawanjohi7923@gmail.com>
2021-06-11 14:08:00 +0300

    done with models

--- shop/models.py
+++ shop/models.py
@@ -1,3 +1,44 @@
 from django.db import models
+from users.models import Profile
 
-# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    price = models.FloatField()
+
+    def __str__(self):
+        return self.name
+
+
+class ProductImage(models.Model):
+    product = models.ForeignKey(Product, related_name="product", on_delete=models.CASCADE)
+    image = models.ImageField(upload_to='product_images')
+
+    def __str__(self):
+        return f'{self.product.name}\'s image {self.id} '
+
+
+class OrderItem(models.Model):
+    customer = models.ForeignKey(Profile, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.IntegerField()
+    total_price = models.FloatField()
+
+    def __str__(self):
+        return f'{self.product.name}\'s order'
+
+
+class Order(models.Model):
+    item = models.ManyToManyField(OrderItem)
+    made_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'order by {self.item.customer.firstname}'
+
--- users/models.py
+++ users/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
-# Create your models here.
+
+class Profile(models.Model):
+    user = models.OneToOneField(get_user_model(), on_delete=models.CASCADE)
+    firstname = models.CharField(max_length=100)
+    lastname = models.CharField(max_length=100)
+    phone = models.CharField(max_length=100)
+    image = models.ImageField(upload_to='profile', default='user.png')
+
+    def __str__(self):
+        return self.firstname
+
+
+@receiver(post_save, sender=get_user_model())
+def create_profile(sender, instance, created, **kwargs):
+    if created:
+        profile = Profile.objects.create(user=instance)
+        profile.save()
